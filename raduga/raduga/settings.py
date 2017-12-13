7419f3729748f33171d42f6cedb76ee19c4c77aa
Antttooon <anttoon@bk.ru>
2017-12-13 23:38:47 +0300

    Templates settings

--- raduga/raduga/settings.py
+++ raduga/raduga/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'raduga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR+'/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
